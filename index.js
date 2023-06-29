a3eee20233faa7b448e334586c05da63eaead887
Nicholas Lee <nicholasleexyz@gmail.com>
2023-06-28 20:23:07 -0600

    eslint

---
+++ index.js
@@ -0,0 +1,9 @@
+/* eslint-disable no-console */
+function AddItem() {
+  console.log('asdf');
+}
+
+const buttonAddItem = document.getElementById('button-add-item');
+// const itemTable = document.getElementById('item-table');
+
+buttonAddItem.onclick = AddItem;
